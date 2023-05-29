0ee030669e9f41244b0ef0f4099ff6f66d18879e
Nandana Nair <2020.nandana.nair@ves.ac.in>
2023-05-30 00:05:57 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Offers from "./pages/Offers";
+import Forgotpsswd from "./pages/Forgotpsswd";
+
+
 function App() {
   return (
-    <div className='text-2xl'>
-      <h1>HELLO WORLD</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Profile" element={<Profile />}></Route>
+        <Route path="/Signin" element={<Signin />}></Route>
+        <Route path="/Signup " element={<Signup />}></Route>
+        <Route path="/Offers" element={<Offers />}></Route>
+        <Route path="/Forgotpsswd" element={<Forgotpsswd />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Forgotpsswd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Forgotpsswd() {
+  return (
+    <div>Forgotpsswd</div>
+  )
+}

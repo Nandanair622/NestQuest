db9ffb8dc02161c2955089d9cbbbf9765b1e2739
Nandana Nair <2020.nandana.nair@ves.ac.in>
2023-06-24 23:41:25 +0530

    Add share button functionality

--- src/pages/DisplayListing.jsx
+++ src/pages/DisplayListing.jsx
@@ -11,7 +11,8 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
-
+import { FaShareAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
 export default function DisplayListing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
@@ -34,32 +35,38 @@ export default function DisplayListing() {
   if (loading) {
     return <Spinner />;
   }
-
+function handleCopyLink() {
+  navigator.clipboard.writeText(window.location.href);
+  toast.success("Link copied");
+}
   return (
     <main>
-      {listing && (
-        <Swiper
-          slidesPerView={1}
-          navigation
-          pagination={{ type: "progressbar" }}
-          effect="fade"
-          autoplay={{ delay: 2000 }}
-        >
-          {listing.imgUrls.map((url, index) => (
-            <SwiperSlide key={index}>
-              <div
-                className="swiper-slide-image"
-                style={{
-                  backgroundImage: `url(${url})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  height: "500px",
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        autoplay={{ delay: 2000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="swiper-slide-image"
+              style={{
+                backgroundImage: `url(${url})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "500px",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-red-400  rounded-full w-12 h-12 flex justify-center items-center"
+      onClick={handleCopyLink}
+  >
+        <FaShareAlt className="text-lg text-red-400"/>
+      </div>
     </main>
   );
 }
